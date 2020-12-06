dfa71cb6267bbd1c38a9fad4c4d24f7864c7e7d7
MignonGakuba <36228508+MignonGakuba@users.noreply.github.com>
2020-12-06 15:44:33 +0100

    XD design

--- reciswipe/components/Main.js
+++ reciswipe/components/Main.js
@@ -1,11 +1,21 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import {View,Text} from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const Main = () => (
-  <View>
+  <View style={styles.container}>
     <Text>Hello World</Text>
   </View>
 );
 
 export default Main;
+
+
+const styles = StyleSheet.create(
+{
+     container:{
+       flex:1,
+       backgroundColor: 'dodgerblue',
+       alignItems: 'center'
+     }
+});
