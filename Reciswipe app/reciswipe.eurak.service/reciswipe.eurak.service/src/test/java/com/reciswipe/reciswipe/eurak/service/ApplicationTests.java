c77caaa6169697b64c98a17e9f6c576545de13bc
MignonGakuba <36228508+MignonGakuba@users.noreply.github.com>
2021-04-07 21:56:30 +0200

    Start of the 4 spring application

---
+++ Reciswipe app/reciswipe.eurak.service/reciswipe.eurak.service/src/test/java/com/reciswipe/reciswipe/eurak/service/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reciswipe.reciswipe.eurak.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
