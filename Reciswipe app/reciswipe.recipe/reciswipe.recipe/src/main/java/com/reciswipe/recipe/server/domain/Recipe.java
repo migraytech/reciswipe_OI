5cc7e6bd52f7ee52c9f95e99a06a79845e906813
MignonGakuba <36228508+MignonGakuba@users.noreply.github.com>
2021-04-13 22:20:30 +0200

    first start of the Recipe domain model

--- Reciswipe app/reciswipe.recipe/reciswipe.recipe/src/main/java/com/reciswipe/recipe/server/domain/Recipe.java
+++ Reciswipe app/reciswipe.recipe/reciswipe.recipe/src/main/java/com/reciswipe/recipe/server/domain/Recipe.java
@@ -1,4 +1,16 @@
 package com.reciswipe.recipe.server.domain;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity(name = "recipes")
 public class Recipe {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+
 }
